a6c087b9280a5d4a8f35f4a4e169ae3b3c19975d
vinaykavuri <kavurivinay@gmail.com>
2020-02-21 14:15:50 -0500

    sdafs

--- app/src/main/java/com/kavurivinay/lab04/MainActivity.java
+++ app/src/main/java/com/kavurivinay/lab04/MainActivity.java
@@ -3,12 +3,26 @@ package com.kavurivinay.lab04;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    int layoutCounter;
+    View[] layouts;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
+        setContentView(R.layout.activity_main1);
+        layouts = [R.layout.activity_main1, R.layout.activity_main2, R.layout.activity_main3, R.layout.acitvity_main4, R.layout.activity_main5, R.layout.activity_main6, R.layout.activity_main7];
+        layoutCounter = 1;
+    }
+
+    public void onClick(View v){
+        layoutCounter++;
+        if(layoutCounter > 7)
+        {
+            layoutCounter = 1;
+        }
+        setContentView(layouts[layoutCounter - 1]);
     }
 }
